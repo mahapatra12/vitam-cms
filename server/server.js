fdf47044ff04e6c59aa3b4d474bfd0d20f37678c
mahapatra12 <mahapatrasoumyaranjan119@gmail.com>
2026-01-17 14:42:20 +0530

    Configure for Vercel Deployment

--- server/server.js
+++ server/server.js
@@ -75,4 +75,8 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+}
+
+module.exports = app;
